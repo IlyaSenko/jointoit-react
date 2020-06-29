ed65d483e4388c82684d9780962d996fe164ec7e
Altair <altair.aquilae42@gmail.com>
2020-06-29 20:56:02 +0300

    Updates

---
+++ precache-manifest.10c9fa9413f2024a316f1c9ce8df0c97.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e69ba8c023f35183d3600499d38251d",
+    "url": "/jointoit-react/index.html"
+  },
+  {
+    "revision": "fbb17df5ec9c5cb695f9",
+    "url": "/jointoit-react/static/css/2.871674e1.chunk.css"
+  },
+  {
+    "revision": "71e203e3e10d83887965",
+    "url": "/jointoit-react/static/css/main.13402fc0.chunk.css"
+  },
+  {
+    "revision": "fbb17df5ec9c5cb695f9",
+    "url": "/jointoit-react/static/js/2.c2fdbb0f.chunk.js"
+  },
+  {
+    "revision": "64676929a30154358d2d7a261320c31d",
+    "url": "/jointoit-react/static/js/2.c2fdbb0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e203e3e10d83887965",
+    "url": "/jointoit-react/static/js/main.04bd7d85.chunk.js"
+  },
+  {
+    "revision": "07799068d4ba25c7eb0b",
+    "url": "/jointoit-react/static/js/runtime-main.9cbcfed0.js"
+  },
+  {
+    "revision": "e1ad04b9073b58c59d433cefe09d161b",
+    "url": "/jointoit-react/static/media/arrow.e1ad04b9.svg"
+  },
+  {
+    "revision": "84b85e8d80d2675c02f9058416639985",
+    "url": "/jointoit-react/static/media/avatar.84b85e8d.png"
+  },
+  {
+    "revision": "ba1f87bb5659a122e1731d29adb84d1c",
+    "url": "/jointoit-react/static/media/badge.ba1f87bb.svg"
+  },
+  {
+    "revision": "9d698f6244c79d299909cd985f518c87",
+    "url": "/jointoit-react/static/media/help.9d698f62.svg"
+  },
+  {
+    "revision": "b209ae3879c9b4940a2c3430de36927c",
+    "url": "/jointoit-react/static/media/home.b209ae38.svg"
+  },
+  {
+    "revision": "1255e31eb96716eea776dd79c94a7fb2",
+    "url": "/jointoit-react/static/media/inbox.1255e31e.svg"
+  },
+  {
+    "revision": "353a2ee34caed38847d25a118a2c173f",
+    "url": "/jointoit-react/static/media/invoices.353a2ee3.svg"
+  },
+  {
+    "revision": "9b15ce11ed6ffff1f5c13e69468566f1",
+    "url": "/jointoit-react/static/media/search.9b15ce11.svg"
+  },
+  {
+    "revision": "732d77b8e125a43a0b8672c17b54fee7",
+    "url": "/jointoit-react/static/media/settings.732d77b8.svg"
+  }
+]);
